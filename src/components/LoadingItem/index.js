b95c89ad2be68464a7768f4d20dc1e4c72575b17
Jonathan <many.d.r.o.2001@hotmail.com>
2025-01-22 11:25:16 -0500

    Added the loading item

--- src/components/LoadingItem/index.js
+++ src/components/LoadingItem/index.js
@@ -2,7 +2,13 @@ import './LoadingItem.css';
 import React from 'react';
 
 function LoadingItem() {
-  return <li className={`loading-item`}></li>;
+  return (
+    <li className={`loading-item`}>
+      <div className="buttons"></div>
+      <div className="text"> </div>
+      <div className="buttons"> </div>
+    </li>
+  );
 }
 
 export { LoadingItem };
