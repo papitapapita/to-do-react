27425dabc454825405de32087d9c8ecaa73b6812
Jonathan <many.d.r.o.2001@hotmail.com>
2025-07-14 16:26:17 -0500

    ToDoList component completely typed

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -1,16 +1,8 @@
+import { ReactNode } from 'react';
 import './ToDoList.css';
-import React from 'react';
-import PropTypes from 'prop-types';
 
-function ToDoList({ children }) {
+function ToDoList({ children }: { children: ReactNode }) {
   return <ul className="to-do-list">{children}</ul>;
 }
-
-ToDoList.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]).isRequired,
-};
 
 export { ToDoList };
